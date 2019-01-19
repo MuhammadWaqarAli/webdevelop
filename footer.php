cb6b7df89946f1ce6e88612ce0882b14daa24df9
karimabbi <karimalimarkatiya@gmail.com>
2019-01-19 01:42:13 -0800

    insilalize our project

---
+++ footer.php
@@ -0,0 +1,77 @@
+
+<div class="container-fluid footer">
+  <div class="row text-center text-sm-left text-md-left">
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+      <h5>
+        <img src="assets/img/download.png" class="img-fluid" height="70" width="70">Company name</h5>
+      <p>this is my company</p>
+    </div>
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+           <h5>Quick Link</h5>
+           <ul class="unstyle-list">
+            <li>
+              <a href="#"><i class="fas fa-angle-double-right"></i> Home</a>
+            </li>
+            <li>
+              <a href="#"><i class="fas fa-angle-double-right"></i> Posrtfollio</a>
+            </li>
+            <li>
+                <a href="#"><i class="fas fa-angle-double-right"></i> Contact</a>
+            </li>
+            <li>
+               <a href="#"><i class="fas fa-angle-double-right"></i> About Us</a>
+            </li>
+           </ul>
+    </div>
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+           <h5>Extra link</h5>
+           <ul class="unstyle-list">
+              <li>
+                <a href="#"><i class="fas fa-angle-double-right"></i> Home</a>
+              </li>
+              <li>
+                <a href="#"><i class="fas fa-angle-double-right"></i> Posrtfollio</a>
+              </li>
+              <li>
+                  <a href="#"><i class="fas fa-angle-double-right"></i> Contact</a>
+              </li>
+              <li>
+                 <a href="#"><i class="fas fa-angle-double-right"></i> About Us</a>
+              </li>
+            </ul>
+    </div>
+  </div>
+  <div class="row" >
+  <div class="col-xs-12 col-md-12 col-lg-12 col-xl-12 text white text-center">
+    <ul class="list-inline social-icon">
+      <li class="list-inline-item"><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+      <li class="list-inline-item">
+        <a href="#">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#">
+          <i class="fab fa-linkedin-in"></i>
+        </a>
+      </li>
+      <li  class="list-inline-item">
+        <a href="#">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+      </ul>
+    </div>
+   </div>
+   <div class="row">
+     <div <div class="col-xs-12 col-md-12 col-lg-12 col-xl-12 text white text-center">
+       <p class="footer-link"><a href="//www.facebook.com" target="-blank" >facebook.com</a>copyright2019</p>
+     </div>
+   </div>
+</div>
+
+
+<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+  <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="assets/js/bootstrap.bundle.js"></script>
+  <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
---
+++ index.php
@@ -0,0 +1,47 @@
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+ 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+ 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+ 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-grid.css">
+ 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-grid.min.css">
+ 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-reboot.css">
+ 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-reboot.min.css">
+ 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+ 	<title> Ui Learning</title>
+ </head>
+ <body>
+ 	<div class="main-contant">
+ 		<h1>Main contant</h1>
+ 		<div>potfolio</div>
+ 	</div>
+  <?php require_once 'footer.php'; ?>
+  </body>
+
+ </html>
+ <script type="text/javascript">
+ 	// var a=10;
+ 	// var a=b=c=10;
+ 	// var a=10;
+ 	// var b=10;
+ 	// var c=10;
+ 	// var data ={'id' :10,'name':'wicky'}
+ 	// var result;
+ 	// var a=54,b=89,c=146;
+ 	// result=a+b+c;
+ 	// console.log(result)
+ 	// divide=a/b/c;
+ 	// console.log(divide)
+ 	// multiply=a*b*c;
+ 	// console.log(multiply)
+ 	// 
+ 	var marks=56;
+ 	if(marks>33){
+ 		console.log("pass");
+     }
+     else
+     	console.log("failed");
+     
+
+ </script>
