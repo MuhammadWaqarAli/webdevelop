031088ee92c8ad4a6ba9645291bcbce0151ac85d
karimabbi <karimalimarkatiya@gmail.com>
2019-01-27 02:01:08 -0800

    fixed bugs and add new work

--- index.php
+++ index.php
@@ -9,10 +9,119 @@
  	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-reboot.css">
  	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-reboot.min.css">
  	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
  	<title> Ui Learning</title>
  </head>
  <body>
  	<div class="main-contant">
- 		<h1>Main contant</h1>
+ 		<h1> Port Folio </h1>
+
+ 		
  	</div>
   <?php require_once 'footer.php'; ?>
+  </body>
+
+ </html>
+ <script type="text/javascript">
+ 	// var a=10;
+ 	// var a=b=c=10;
+ 	// var a=10;
+ 	// var b=10;
+ 	// var c=10;
+ 	// var data ={'id' :10,'name':'wicky'}
+ 	// var result;
+ 	// var a=54,b=89,c=146;
+ 	// result=a+b+c;
+ 	// console.log(result)
+ 	// divide=a/b/c;
+ 	// console.log(divide)
+ 	// multiply=a*b*c;
+ 	// console.log(multiply)
+ 	// 
+ 	// var marks=56;
+ 	// if(marks>33){
+ 	// 	console.log("pass");
+  //    }
+  //    else
+  //    	console.log("failed");
+     //
+    
+     // console.log( 'Banking System');
+     // console.log( ' press 1 for inquiry');
+     // console.log( '  press 2 for Withdrow');
+     // console.log( ' press 3 for Transfore');
+     // console.log( ' press 4 for Exit');
+      
+     //  var optione = 1;
+     //   switch(optione){
+     //   	case 1 : 
+     //   		console.log('Welcome to Inquiry');
+     //   		break;
+     //   	case 2 :
+     //   	    console.log('Welcome to Withdrow')
+     //   	    break;
+     //   	case 3 :
+     //   	     console.log('Welcome to Transfore')
+     //   	     break;
+     //   	case 4 :
+     //   	     console.log('Exit')
+     //   	     break;
+     //    default:
+     //   		console.log('Invalid Option');
+     //   		break;
+// }
+    
+    // var i=10
+    // while (i>=1){
+    // i--
+    // }
+    function add ( ){
+    	var a =10;
+    	var b =5;
+    	var add = a+b;
+    	console.log(add);
+    	}
+    	// add();
+    	function sub ( ){
+    		var a =15;
+    		var b =10;
+    		var sub =a-b;
+    		console.log( sub);
+    	}
+    // 	sub ();
+    function multiply ( ){
+    		var a =15;
+    		var b =10;
+    		var multiply =a*b;
+    		console.log( multiply);
+    	}
+    	function divide ( ){
+    		var a =15;
+    		var b =10;
+    		var divide =a/b;
+    		console.log( divide);
+    	}
+     var a=10
+    var b=5
+    var option ='divide'
+    switch (option){
+    	case 'add':
+    add();
+    break;
+    case 'sub':
+    sub();
+    break;
+    case 'multiply':
+    multiply();
+    break;
+    case 'divide':
+    divide();
+    break;
+    console.log('invaled Input')
+    } 
+
+
+    </script>        
+
+
+
